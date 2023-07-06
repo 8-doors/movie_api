a46dbcb979e8d1d4f0111d0dfe9b65cac9cd2831
Brandon Sanoski <blacklightning123017@gmail.com>
2023-07-06 12:34:51 -0600

    conected database to heroku

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use(morgan('common'))
 
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb://127.0.0.1:27017/f_stop', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 /*CREATE
   We’ll expect JSON in this format
